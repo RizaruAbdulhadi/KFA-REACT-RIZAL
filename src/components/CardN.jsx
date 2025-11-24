3f04f4055c9bed5aee52a1bb436b207d4a5bf088
unknown <cerberus16121992@gmail.com>
2025-11-24 16:34:42 +0700

    update perbaikan halaman baru

--- src/components/CardN.jsx
+++ src/components/CardN.jsx
@@ -1,4 +1,4 @@
-function CardNews() {
+export function CardNews() {
   return (
     <div className="card">
       <img src="..." className="card-img-top" alt="..." />
@@ -16,4 +16,38 @@ function CardNews() {
   );
 }
 
-export default CardNews;
+export function CardNews2() {
+  return (
+    <div className="card">
+      <img src="..." className="card-img-top" alt="..." />
+      <div className="card-body">
+        <h5 className="card-title">Card title2</h5>
+        <p className="card-text">
+          Some quick example text to build on the card title and make up the
+          bulk of the card’s content.
+        </p>
+        <a href="#" className="btn btn-primary">
+          Go somewhere
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export function CardNews3() {
+  return (
+    <div className="card">
+      <img src="..." className="card-img-top" alt="..." />
+      <div className="card-body">
+        <h5 className="card-title">Card title3</h5>
+        <p className="card-text">
+          Some quick example text to build on the card title and make up the
+          bulk of the card’s content.
+        </p>
+        <a href="#" className="btn btn-primary">
+          Go somewhere
+        </a>
+      </div>
+    </div>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import Carousel from "../components/Carousel";
-import CardNews from "../components/CardN";
+import { CardNews, CardNews2, CardNews3 } from "../components/CardN";
 
 function Home() {
   return (
@@ -7,18 +7,22 @@ function Home() {
       <Carousel />
 
       {/* section cards news */}
-      <section className="card-news">
-        <div className="container text-center">
-          <div className="row justify-content-center align-items-center">
-            <div className="col">Company News</div>
-          </div>
-        </div>
-      </section>
-
       <div className="container">
-        <div className="row">
-          <div className="col">
-            <CardNews />
+        <section className="card-news">
+          <div className="container text-center">
+            <div className="row justify-content-center align-items-center">
+              <div className="col">Company News</div>
+            </div>
+          </div>
+        </section>
+
+        <div className="container">
+          <div className="row">
+            <div className="col-md-4">
+              <CardNews />
+              <CardNews2 />
+              <CardNews3 />
+            </div>
           </div>
         </div>
       </div>
